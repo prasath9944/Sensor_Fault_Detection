050ef523c8c365b4a5a8692b59464549e690fedd
prasath9944 <prasathks20@gmail.com>
2023-02-05 23:38:54 +0530

    Updated Docker

--- data_dump.py
+++ data_dump.py
@@ -1,8 +1,9 @@
 import pymongo
 import pandas as pd
 import json
+from sensor.config import mongo_client
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
@@ -20,7 +21,7 @@ if __name__=="__main__":
     print(json_record[0])
 
     #insert converted json record to mongo db
-    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
 
 
 
